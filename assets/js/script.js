f28761bd0426de4e826817c6427553a99974be55
begin <osefashviligvanca@gmail.com>
2019-07-14 22:45:26 +0400

    no message

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $("#navbar-slide").click(function() {
+    $(".hide-panel").slideDown();
+  });
+});
+
+$("#navbar-slide-two").click(function() {
+  $(".hide-panel").slideUp();
+});
